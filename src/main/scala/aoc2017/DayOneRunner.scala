18dadceaf0c7727bf4ec736390b1ec2e3cd4812b
Stanislaw Wisniewski <Stanislaw.Wisniewski@pega.com>
2023-11-30 17:35:52 +0100

    2017 Day One, nicer

--- src/main/scala/aoc2017/DayOneRunner.scala
+++ src/main/scala/aoc2017/DayOneRunner.scala
@@ -9,17 +9,16 @@ class DayOneRunner(inputPathname: String) extends AocRunner(inputPathname) {
   override val input: List[Int] = source.mkString.toList.map(_.toString.toInt)
 
   override def part1(): String =
-    accumulate(input).toString
+    val tuples = input.lazyZip(input.drop(1) ++ input.take(1))
+    accumulate(tuples).toString
 
   @tailrec
-  private def accumulate(xs: List[Int], sum: Int = 0): Int =
-    if xs.tail.isEmpty then return sum + (if xs.head == input.head then xs.head else 0)
-    if xs.head == xs.tail.head then accumulate(xs.tail, sum + xs.head) else accumulate(xs.tail, sum)
+  private def accumulate(xs: Iterable[(Int, Int)], sum: Int = 0): Int =
+    if xs.isEmpty then return sum
+    accumulate(xs.drop(1), if xs.head(0) == xs.head(1) then sum + xs.head(0) else sum)
 
   override def part2(): String =
     val len = input.length
-    var sum = 0
-    for i <- 0 until len do
-      if input(i) == input((i + len/2) % len) then sum = sum + input(i)
-    sum.toString
+    val tuples = input.lazyZip(input.drop(len/2) ++ input.take(len/2))
+    accumulate(tuples).toString
 }
