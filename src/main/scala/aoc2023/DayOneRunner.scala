7cbcc51f80892975fb83e99207b42c043e767539
Stanislaw Wisniewski <Stanislaw.Wisniewski@pega.com>
2023-12-01 11:13:23 +0100

    Remove puzzle text just in case of any copyright issues

--- src/main/scala/aoc2023/DayOneRunner.scala
+++ src/main/scala/aoc2023/DayOneRunner.scala
@@ -11,36 +11,15 @@ class DayOneRunner(inputPathname: String) extends AocRunner(inputPathname) {
   private def digitsR: Regex = raw"(zero|one|two|three|four|five|six|seven|eight|nine)".r
 
   /**
-   * <p/>The newly-improved calibration document consists of lines of text; each line originally contained a specific calibration value that the Elves now need to recover. On each line, the calibration value can be found by combining the first digit and the last digit (in that order) to form a single two-digit number.
-   * <p/>For example:
-   * <pre>
-   * 1abc2
-   * pqr3stu8vwx
-   * a1b2c3d4e5f
-   * treb7uchet
-   * </pre>
-   * <p/>In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
-   * <p/>Consider your entire calibration document. What is the sum of all of the calibration values?
-   * */
+   *  See puzzle at https://adventofcode.com/2023/day/1
+   */
   override def part1(): String =
     input.map(
       _.filter(_.isDigit).headAndTail.map(_.toString).foldRight("")(_++_).toInt
     ).sum.toString
 
   /**
-   * <p/>Your calculation isn't quite right. It looks like some of the digits are actually spelled out with letters: one, two, three, four, five, six, seven, eight, and nine also count as valid "digits".
-   * <p/>Equipped with this new information, you now need to find the real first and last digit on each line. For example:
-   * <pre>
-   * two1nine
-   * eightwothree
-   * abcone2threexyz
-   * xtwone3four
-   * 4nineeightseven2
-   * zoneight234
-   * 7pqrstsixteen
-   * </pre>
-   * <p/>In this example, the calibration values are 29, 83, 13, 24, 42, 14, and 76. Adding these together produces 281.
-   * <p/>What is the sum of all of the calibration values?
+   * See puzzle at https://adventofcode.com/2023/day/1
    */
   override def part2(): String =
     input
